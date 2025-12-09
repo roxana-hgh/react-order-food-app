2bafbeba8367313f8422005394745b8883845522
roxana-hgh <haghgoo.roxana@gmail.com>
2025-12-09 20:41:48 +0330

    submit text added to button

--- src/components/foods-menu/food-card/foodCardForm.jsx
+++ src/components/foods-menu/food-card/foodCardForm.jsx
@@ -3,16 +3,23 @@ import { Fragment , useRef, useState } from "react";
 function CardForm(prop) {
     const amountValueRef = useRef()
     const [amountIsValid , setAmountIsValid] = useState(true)
+    const [isAdded , setIsAdded] = useState(false)
     
 
     const SubmitHandler = (event) => {
         event.preventDefault()
+        
         const amount = +amountValueRef.current.value
         if( amount < 1 && amount > 5 ) {
             setAmountIsValid(false)
             return
         }
         prop.onFormSubmit(amount)
+        setIsAdded(true)
+        setTimeout(() => {
+            setIsAdded(false)
+        }, 1000);
+        
     }
     return ( 
         <Fragment>
@@ -24,7 +31,7 @@ function CardForm(prop) {
                        
                         <input ref={amountValueRef} id={`amount-${prop.id}`}  type="number" min='1' defaultValue='1'  className="border w-full rounded-full px-2 m-0  " />
                     </div>
-                <button className="px-4 py-1 bg-orange-600 text-orange-50 border-orange-600 rounded-full w-3/4 block ">Add to card</button>
+                <button className="px-4 py-1 bg-orange-600 text-orange-50 border-orange-600 rounded-full w-3/4 block ">{isAdded ? "Added!" : "Add to Card"}</button>
                 </div>
                 {!amountIsValid && <span>Pleas Enter a valid amount!</span> }
             </form>
